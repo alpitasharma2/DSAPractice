c0234fe8f930ae22963903d712cf912090071d1b
Alpita Sharma <alpita0908.dev@gmail.com>
2024-12-05 16:23:15 +0530

    Update intersectionoflinkedlist.js

--- intersectionoflinkedlist.js
+++ intersectionoflinkedlist.js
@@ -9,3 +9,29 @@ var getIntersectionNode = function(headA, headB) {
     // if a reaches to the end of the list then it will point to headB and if not a = a.next then it will keep moving.
     return a
 };
+
+// 2 
+var getIntersectionNode = function(headA, headB) {
+    if(headA === null || headB === null) return null
+
+    let p = headA;
+    let q = headB;
+
+    while(p!==q)
+    {
+        if(p === null)
+        {
+            p = headB;
+        }else{
+            p = p.next;
+        }
+
+        if(q === null)
+        {
+            q = headA;
+        }else{
+            q = q.next;
+        }
+    }
+    return p;
+};
